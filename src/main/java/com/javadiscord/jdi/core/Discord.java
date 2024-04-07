20150c89d4480d47af0a3702e2412f8d3c07533b
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-04-07 23:40:19 +0800

    add documentation for Discord and Gateway classes

--- src/main/java/com/javadiscord/jdi/core/Discord.java
+++ src/main/java/com/javadiscord/jdi/core/Discord.java
@@ -16,17 +16,69 @@ import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+/**
+ * The core of JavaDiscordAPI. It manages gateway connections to Discord with a
+ * {@link com.javadiscord.jdi.internal.gateway.WebSocketManager} and runs a
+ * {@link com.javadiscord.jdi.internal.api.DiscordRequestDispatcher}.
+ *
+ * @author Suraj Kumar
+ * @since 1.0.0
+ */
 public class Discord {
+    /**
+     * An instance of a {@link org.apache.logging.log4j.Logger} for diagnostics
+     * and logging purposes.
+     */
     private static final Logger LOGGER = LogManager.getLogger();
+    /**
+     * An instance of a {@link java.util.concurrent.Executor} for managing
+     * {@link java.lang.Runnable} tasks.
+     */
     private static final Executor EXECUTOR = Executors.newSingleThreadExecutor();
+    /**
+     * An {@link com.fasterxml.jackson.databind.ObjectMapper} for reading and writing
+     * JSON from Java objects.
+     */
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    /**
+     * The official website of JavaDiscordAPI. This is used when identifying with Discord
+     * through the WebSocket gateway.
+     */
     private static final String WEBSITE = "https://javadiscord.com/";
+    /**
+     * The OAuth token of the bot that is required for authentication when using most
+     * endpoints of the Discord API.
+     */
     private final String botToken;
+    /**
+     * The identify request payload sent to Discord after establishing a connection
+     * to the WebSocket gateway.
+     */
     private final IdentifyRequest identifyRequest;
+    /**
+     * An HTTP request dispatcher for sending HTTP requests to the Discord API
+     * endpoints.
+     */
     private final DiscordRequestDispatcher discordRequestDispatcher;
+    /**
+     * The JSON payload resulting from a request to the <a href="https://discord.com/developers/docs/topics/gateway#get-gateway-bot">Get Gateway Bot</a>
+     * endpoint.
+     */
     private final Gateway gateway;
+    /**
+     * Configuration options for specifying compression algorithm and API version for
+     * the WebSocket gateway.
+     */
     private final GatewaySetting gatewaySetting;
 
+    /**
+     * Constructs a new Discord client with the specified bot token.
+     * <p>
+     * By default, this constructs a client enabling all of the
+     * <a href="https://discord.com/developers/docs/topics/gateway#gateway-intents">Gateway Intents</a>.
+     *
+     * @param botToken The bot token to use for this instance of the client.
+     */
     public Discord(String botToken) {
         this(
                 botToken,
@@ -46,6 +98,13 @@ public class Discord {
                         .build());
     }
 
+    /**
+     * Constructs a new Discord client with the specified bot token and list of
+     * {@link com.javadiscord.jdi.internal.gateway.GatewayIntent}s.
+     *
+     * @param botToken The bot token to use for this instance of the client.
+     * @param intents The list of <a href="https://discord.com/developers/docs/topics/gateway#gateway-intents">Gateway Intents</a> to use.
+     */
     public Discord(String botToken, List<GatewayIntent> intents) {
         this(
                 botToken,
@@ -65,6 +124,13 @@ public class Discord {
                         .build());
     }
 
+    /**
+     * Constructs a new Discord client with the specified bot token and a custom
+     * {@link com.javadiscord.jdi.internal.gateway.identify.IdentifyRequest} payload.
+     *
+     * @param botToken The bot token to use for this instance of the client.
+     * @param identifyRequest The custom identify request payload to use for this instance.
+     */
     public Discord(String botToken, IdentifyRequest identifyRequest) {
         this.botToken = botToken;
         this.discordRequestDispatcher = new DiscordRequestDispatcher(botToken);
@@ -74,6 +140,10 @@ public class Discord {
         this.identifyRequest = identifyRequest;
     }
 
+    /**
+     * Initiates a Discord gateway connection and starts a background task for the
+     * request dispatcher.
+     */
     public void start() {
         WebSocketManager webSocketManager =
                 new WebSocketManager(
@@ -91,6 +161,12 @@ public class Discord {
         EXECUTOR.execute(discordRequestDispatcher);
     }
 
+    /**
+     * Obtains the preferred gateway configuration for establishing the connection.
+     *
+     * @param authentication The bot token for use.
+     * @return The preferred gateway configuration returned from Discord.
+     */
     private static Gateway getGatewayURL(String authentication) {
         try (HttpClient httpClient = HttpClient.newBuilder().build()) {
             HttpRequest request =
--- src/main/java/com/javadiscord/jdi/internal/gateway/Gateway.java
+++ src/main/java/com/javadiscord/jdi/internal/gateway/Gateway.java
@@ -3,6 +3,17 @@ package com.javadiscord.jdi.internal.gateway;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * A preferred set of configuration for usage when connecting to the Discord WebSocket gateway.
+ * <p>
+ * This object is returned by calling the <a href="https://discord.com/developers/docs/topics/gateway#get-gateway-bot">Get Gateway Bot</a>
+ * endpoint.
+ *
+ * @param url WebSocket URL that can be used for connecting to the Discord gateway.
+ * @param shards Recommended number of <a href="https://discord.com/developers/docs/topics/gateway#sharding">shards</a>
+ *               to use when connecting to the Discord gateway.
+ * @param sessionStartLimit Information on the current session start limit.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Gateway(
         String url,
